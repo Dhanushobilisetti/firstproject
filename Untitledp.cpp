55d8bd3bb958631f713c4f83a7a93cdf7dbfb94a
OBHILISETTI DHANUSH <dhanushobilisetti@gmail.com>
2025-06-06 10:13:07 +0530

    yes

---
+++ Untitledp.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+inline int triangle(int h,int b)
+{
+	return(0.5*b*h);
+}
+int main()
+{
+	int b,h;
+	cout<<"Enter base: ";
+	cin>>b;
+	cout<<"Enter height: ";
+	cin>>h;
+}
